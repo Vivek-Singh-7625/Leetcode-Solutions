ac911db78a40992ccbee87fa71c5e0943a0a2777
Vivek-Singh-7625 <vs514892@gmail.com>
2026-07-19 13:28:57 +0530

    Time: 11 ms (79.53%), Space: 10.1 MB (94.11%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -2,23 +2,14 @@ class Solution {
 public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-
-        vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
-
-        for (int i = 0; i < n; i++) dp[i][0] = 1;
-
-        for (int j = 0; j <= amount; j++){
-            if (j % coins[0] == 0)  dp[0][j] = 1;
-        }
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j <= amount; j++) {
-                dp[i][j] = dp[i - 1][j];
-                if (j >= coins[i]) {
-                    if (dp[i][j] > INT_MAX - dp[i][j - coins[i]])   dp[i][j] = INT_MAX;
-                    else    dp[i][j] += dp[i][j - coins[i]];
-                }
+        vector<int> dp(amount + 1, 0);
+        dp[0] = 1;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = coins[i] ; j <= amount ; j++){
+                if(dp[j] > INT_MAX - dp[j-coins[i]])    dp[j] = INT_MAX;
+                else    dp[j] += dp[j-coins[i]];
             }
         }
-        return dp[n - 1][amount];
+        return dp[amount];
     }
 };
