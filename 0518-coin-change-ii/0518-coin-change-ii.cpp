48bb910734edcd331b717acb75471c8d57c1a820
Vivek-Singh-7625 <vs514892@gmail.com>
2026-07-19 13:15:42 +0530

    Time: 31 ms (49.54%), Space: 36.4 MB (62.82%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -2,15 +2,23 @@ class Solution {
 public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n,vector<int> (amount+1,-1));
-        return solve(amount , coins , coins.size()-1 , dp);
-    }
-    int solve(int coin , vector<int>& coins , int idx , vector<vector<int>>& dp){
-        if(coin < 0 or idx < 0)    return 0;
-        if(coin == 0)   return dp[idx][coin] = 1;
-        if(dp[idx][coin] != -1) return dp[idx][coin];
-        int pick = solve(coin - coins[idx], coins , idx , dp);
-        int not_pick = solve(coin, coins , idx-1 , dp);
-        return dp[idx][coin] = pick + not_pick ; 
+
+        vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
+
+        for (int i = 0; i < n; i++) dp[i][0] = 1;
+
+        for (int j = 0; j <= amount; j++){
+            if (j % coins[0] == 0)  dp[0][j] = 1;
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j >= coins[i]) {
+                    if (dp[i][j] > INT_MAX - dp[i][j - coins[i]])   dp[i][j] = INT_MAX;
+                    else    dp[i][j] += dp[i][j - coins[i]];
+                }
+            }
+        }
+        return dp[n - 1][amount];
     }
 };
