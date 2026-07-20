3cc66b1f1289502cfcb5854ca6d86a14d99201b4
Vivek-Singh-7625 <vs514892@gmail.com>
2026-07-20 14:34:55 +0530

    Time: 7 ms (19.5%), Space: 18.7 MB (22.37%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
-        int m = grid.size() , n = grid[0].size();
-        int temp;
-        for(int r = 0 ; r < k ; r++){
-            temp = grid[m-1][n-1];
-            for(int i = m-1 ; i >= 0 ; i--){
-                for(int j = n-1 ; j >= 0 ; j--){
-                    if(i != 0 or j != 0){
-                        if(j)   grid[i][j] = grid[i][j-1];
-                        else    grid[i][0] = grid[i-1][n-1];
-                    }
-                    else    grid[0][0] = temp;
+        int m = grid.size() , n = grid[0].size() , x = 0;
+        k = k%(m * n);
+        vector<int> t;
+        stack<int> A;
+        for(int i = m-1 ; i >= 0 ; i--){
+            for(int j = n-1 ; j >= 0 ; j--){
+                if(x < k){
+                    x++;
+                    t.push_back(grid[i][j]);
+                }
+                A.push(grid[i][j]);
+            }
+        }
+        int y = t.size();
+        x = 0;
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(x++ < k)   grid[i][j] = t[--y];
+                else{    
+                    grid[i][j] = A.top();
+                    A.pop();
                 }
             }
         }
