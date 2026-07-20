cc6b3b324cd7dee13604cbaeb5fe78db2de201ee
Vivek-Singh-7625 <vs514892@gmail.com>
2026-07-20 14:08:33 +0530

    Time: 12 ms (10.13%), Space: 18.1 MB (91.78%) - LeetHub

---
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        int m = grid.size() , n = grid[0].size();
+        int temp;
+        for(int r = 0 ; r < k ; r++){
+            temp = grid[m-1][n-1];
+            for(int i = m-1 ; i >= 0 ; i--){
+                for(int j = n-1 ; j >= 0 ; j--){
+                    if(i != 0 or j != 0){
+                        if(j)   grid[i][j] = grid[i][j-1];
+                        else    grid[i][0] = grid[i-1][n-1];
+                    }
+                    else    grid[0][0] = temp;
+                }
+            }
+        }
+        return grid;
+    }
+};
