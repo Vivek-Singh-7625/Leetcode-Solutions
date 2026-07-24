bc6c92e2b70c77e17c96d9ae40b12807467dbfc5
Vivek-Singh-7625 <vs514892@gmail.com>
2026-07-24 21:08:47 +0530

    Time: 393 ms (84.27%), Space: 35.2 MB (81.46%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
-        int ans = 0 , n = nums.size() , t;
-        unordered_map<int,int> mpp;
-        vector<int> X;
-        vector<bool> V(2049,false);
-        for(int i = 0 ; i < n; i++){
-            for(int j = i ; j < n; j++){
+        int ans = 0 , n = nums.size() , t , x;
+        vector<bool> A(2048) , B(2048);
+        for(int i = 0 ; i < n ; i++){
+            for(int j = i ; j < n ; j++){
                 t = nums[i]^nums[j];
-                if(!V[t]) X.push_back(t);
-                V[t] = true;
-            }
-        }
-        int n2 = X.size();
-        for(int i = 0 ; i < n; i++){
-            for(int j = i ; j < n2; j++){
-                t = X[j]^nums[i];
-                if(!mpp[t]) ans++;
-                else continue;
-                mpp[t]++;
+                if(A[t] == true)    continue;
+                A[t] = true;
+                for(int k = j ; k < n ; k++){
+                    x = t^nums[k];
+                    if(!B[x])   ans++;
+                    B[x] = true;
+                }
             }
         }
         return ans;
