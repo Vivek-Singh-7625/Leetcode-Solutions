c2fe536a10f41c1bae5ec7a308b76e2a6611413f
Vivek-Singh-7625 <vs514892@gmail.com>
2026-07-25 19:55:44 +0530

    Time: 0 ms (100%), Space: 60.2 MB (13.69%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -4,9 +4,9 @@ public:
         int n = nums.size() , st = 0 , cz = 0 , ans = 0;
         for(int i = 0 ; i < n ; i++){
             if(nums[i] == 0)   cz++; 
-            if(cz > 1){
-                while(st < n-1 and nums[st++] != 0);
-                cz--;
+            while(cz > 1){
+                if(nums[st] == 0)   cz--;
+                st++;
             }
             ans = max(ans,i-st);
         }
