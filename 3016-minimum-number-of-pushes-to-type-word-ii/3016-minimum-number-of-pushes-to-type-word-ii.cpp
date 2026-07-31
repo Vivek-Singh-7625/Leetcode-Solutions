e94e21f10ca84c46b0deb2825834a86b63696a66
Vivek-Singh-7625 <vs514892@gmail.com>
2026-07-31 15:33:23 +0530

    Time: 8 ms (61.48%), Space: 25.5 MB (72.47%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -4,7 +4,11 @@ public:
         vector<int> A(26,0);
         int n = word.size() , ans = 0;
         for(int i = 0 ; i < n ; i++)    A[word[i]-'a']++;
-        sort(A.begin(),A.end(),greater<int>());
+        for(int i = 1; i < 26; i++){
+		    for(int j = i; j > 0 && A[j] > A[j-1]; j--){
+			    swap(A[j],A[j-1]);
+		    }
+	    }
         for(int i = 0 ; i < 26 ; i++)   ans += (i/8 + 1)*A[i];
         return ans;
     }
