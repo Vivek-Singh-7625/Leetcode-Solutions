1076c20af1ae9b9f58841c7dd58e8e57c521b53b
Vivek-Singh-7625 <vs514892@gmail.com>
2026-07-31 15:55:51 +0530

    Time: 0 ms (100%), Space: 9 MB (79.77%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -4,13 +4,13 @@ public:
         string ans;
         int n = num1.length() , m = num2.length() , carry = 0;
         int l = max(m,n) , x;
-        for(int i = 0 ; i < l + 1 ; i++){
+        for(int i = 0 ; i < l ; i++){
             x = (n > 0 ?  num1[--n]-'0' : 0) + (m > 0 ? num2[--m] - '0' : 0) + carry;
             carry = x/10;
             ans += ('0'+x%10);
         }
+        if (carry == 1) ans += '1';
         reverse(ans.begin(),ans.end());
-        while(ans[0] == '0' and ans.size() > 1)  ans.erase(0,1);
         return ans;
     }
 };
