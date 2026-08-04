b2c6090e4fc0bf1db4b7cc4615658889d004db33
Vivek-Singh-7625 <vs514892@gmail.com>
2026-08-04 23:15:18 +0530

    Time: 0 ms (100%), Space: 17.2 MB (50.56%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
-        unordered_map<int, vector<int>> temp_group;
-        vector<vector<int>> result;
-        
-        for(int i = 0; i < groupSizes.size(); ++i) {
-            int size = groupSizes[i];
-            temp_group[size].push_back(i);
-            
-            if(temp_group[size].size() == size) {
-                result.push_back(temp_group[size]);
-                temp_group[size].clear();
-            }
+        unordered_map<int,vector<int>> mpp;
+        int n = groupSizes.size() , x , y , k;
+        for(int i = 0 ; i < n ; i++){
+            mpp[groupSizes[i]].push_back(i);
         }
-        return result;
+        vector<vector<int>> ans;
+        for(auto& [key,value] : mpp){
+            n = value.size();
+            x = key;
+            k = 0;
+            vector<int> temp;
+            for (int val : mpp[key]) {
+                temp.push_back(val);
+                if (temp.size() == key) {
+                    ans.push_back(temp);
+                    temp.clear();
+                }
+            }
+            if(temp.size())   ans.push_back(temp);
+        }
+        return ans;
     }
 };
