add648c75563455f29ba75e1b7e0101659800d7c
Vivek-Singh-7625 <vs514892@gmail.com>
2026-08-04 22:21:29 +0530

    Time: 0 ms (100%), Space: 50.2 MB (62.98%) - LeetHub

--- 3668-restore-finishing-order/3668-restore-finishing-order.cpp
+++ 3668-restore-finishing-order/3668-restore-finishing-order.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     vector<int> recoverOrder(vector<int>& order, vector<int>& friends) {
-        unordered_map<int,int> mpp;
+        int n = order.size();
+        vector<bool> v(n, false);
+        for(int i : friends)    v[i-1] = true;
         int k = 0;
-        for(int i = 0 ; i < friends.size() ; i++)    mpp[friends[i]]++;
-        for(int i = 0 ; i < order.size() ; i++){
-            if(mpp[order[i]])    friends[k++] = order[i];
+        for(int i = 0; i < n; i++){
+            if(v[order[i]-1]) friends[k++] = order[i];
         }
-        return  friends;
+        return friends;
     }
 };
