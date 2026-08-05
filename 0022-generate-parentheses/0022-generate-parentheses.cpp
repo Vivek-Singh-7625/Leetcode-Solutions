d3e3fc78f09aeee475006813630cfd3086d553c5
Vivek-Singh-7625 <vs514892@gmail.com>
2026-08-05 23:37:44 +0530

    Time: 3 ms (69.11%), Space: 15.7 MB (41.68%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -11,6 +11,10 @@ public:
             ans.push_back(t);
             return ;
         }
+        if(left == right){   
+            t += "(";
+            left--;
+        }
         if(left)    helper(left - 1 , right , t + "(" , ans);
         if(right > left)   helper(left , right - 1 , t + ")" , ans);
     }
