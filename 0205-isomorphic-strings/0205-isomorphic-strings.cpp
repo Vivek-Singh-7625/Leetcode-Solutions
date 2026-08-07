7c62395eec7bb3da1c73c2da4962e09022d0d75e
Vivek-Singh-7625 <vs514892@gmail.com>
2026-08-07 16:24:09 +0530

    Time: 0 ms (100%), Space: 9.3 MB (58.74%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<int,int> mpp;
-        unordered_map<int,int> mp;
+        unordered_map<char,char> mpp;
+        unordered_map<char,char> mp;
         int n = s.size();
         for(int i = 0 ; i < n ; i++){
             if(mpp[s[i]] == 0)  mpp[s[i]] = t[i] + 1;
