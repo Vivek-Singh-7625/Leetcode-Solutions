0acf47811f7373dc0b3404abfde3216b4c89dbd3
Vivek-Singh-7625 <vs514892@gmail.com>
2026-08-13 20:31:24 +0530

    Time: 461 ms (11.19%), Space: 26 MB (27.46%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int max_len = 0 , sp = 0 , len;
-        for(int i = 0; i < s.length() ; i++){
+        int max_len = 0 , sp = 0 , len , n = s.length();
+        vector<vector<bool>> dp(n,vector<bool>(n));
+        for(int i = 0; i < n ; i++){
             len = 0;
-            for(int j = i ; j < s.length() ; j++){
+            for(int j = i ; j < n ; j++){
                 len++;
                 if(s.length()-i < max_len)   break;
-                if(solve(i,j,s) == true){
+                if(solve(i,j,s,dp) == true){
                     if(max_len < len){
                         max_len = len;
                         sp = i;
@@ -17,11 +18,12 @@ public:
         }
         return s.substr(sp,max_len);
     }
-    bool solve(int& x , int& y , string& s){
+    bool solve(int& x , int& y , string& s , vector<vector<bool>>& dp){
         int len = y - x + 1;
         for(int i = 0 ; i < len/2 ; i++){
+            if(dp[x+i][y-i])    break;
             if(s[x+i] != s[y-i])    return false;
         }
-        return true;
+        return dp[x][y] = true;
     }
 };
