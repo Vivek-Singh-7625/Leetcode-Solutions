c0821b4b8874419826f95818e517c08e7667389a
Vivek-Singh-7625 <vs514892@gmail.com>
2026-08-14 19:16:38 +0530

    Time: 0 ms (100%), Space: 8 MB (38.41%) - LeetHub

---
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int MOD = 1e9 + 7;
+    int countGoodNumbers(long long n) {
+        unsigned long long y = 1;
+        y = (1ll*myPow(4,n/2)*myPow(5,n-n/2))%MOD;
+        return y;
+    }
+    int myPow(int x, long long n) {
+        if(n == 0)  return 1;
+        if(n == 1)  return x;
+        int t = myPow(x,abs(n/2));
+        if(n%2) return (1ll*t*t*x)%MOD;
+        return (1ll*t*t)%MOD;
+    }
+};
