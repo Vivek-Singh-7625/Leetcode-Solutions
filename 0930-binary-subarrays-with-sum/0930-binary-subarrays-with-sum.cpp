a2d3e85ebf806ea5cfcf6fe0c95feb494becd6d5
Vivek-Singh-7625 <vs514892@gmail.com>
2026-08-20 23:16:06 +0530

    Time: 1999 ms (5.25%), Space: 32.7 MB (62.89%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -4,6 +4,7 @@ public:
         int n = nums.size();
         int t = 0 , sum = 0 , ans = 0;
         for(int i = 1 ; i < n ; i++)    nums[i] += nums[i-1];
+        if(nums[n-1] < goal)    return 0;
         if(nums[n-1] == 0){
             if(goal == 0)  return n*(n+1)/2;
             else    return 0;
