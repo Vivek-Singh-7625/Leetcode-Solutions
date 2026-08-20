ff6b8212e06199b968eb0f060158196c3f2c4815
Vivek-Singh-7625 <vs514892@gmail.com>
2026-08-20 23:14:40 +0530

    Time: 1973 ms (5.25%), Space: 32.8 MB (40.15%) - LeetHub

---
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int n = nums.size();
+        int t = 0 , sum = 0 , ans = 0;
+        for(int i = 1 ; i < n ; i++)    nums[i] += nums[i-1];
+        if(nums[n-1] == 0){
+            if(goal == 0)  return n*(n+1)/2;
+            else    return 0;
+        }  
+        for(int i = 0 ; i < n ; i++){
+            t = i == 0 ? 0 : nums[i-1];
+            for(int j = i ; j < n ; j++)   if(nums[j]-t == goal) ans++;
+        }
+        return ans;
+    }
+};
