ea7830ae1035495dcf846ad51984089f3f12d4d4
Vivek-Singh-7625 <vs514892@gmail.com>
2026-08-23 16:15:48 +0530

    Time: 95 ms (57.88%), Space: 242 MB (93.54%) - LeetHub

--- 4031-find-all-numbers-disappeared-in-an-array-ii/4031-find-all-numbers-disappeared-in-an-array-ii.cpp
+++ 4031-find-all-numbers-disappeared-in-an-array-ii/4031-find-all-numbers-disappeared-in-an-array-ii.cpp
@@ -3,15 +3,15 @@ public:
     vector<vector<int>> findDisappearedNumbers(vector<int>& nums, int lower, int upper) {
         int n = nums.size() , p = lower;
         vector<vector<int>> ans;
-        set<int> s;
-        for(int i = 0 ; i < n ; i++)    s.insert(nums[i]);
-        for(int x : s){
-            if(x < lower) continue;
-            if(x > upper) break;
-            if(x == p)    p = x + 1;
+        sort(nums.begin(),nums.end());
+        for(int i = 0 ; i < n ; i++){
+            if(nums[i] < lower) continue;
+            if(nums[i] > upper) break;
+            if(i and nums[i] == nums[i-1]) continue;
+            if(nums[i] == p)    p = nums[i] + 1;
             else{
-                ans.push_back({p,x-1});
-                p = x + 1;
+                ans.push_back({p,nums[i]-1});
+                p = nums[i] + 1;
             }
         }
         if(p <= upper)   ans.push_back({p,upper});
