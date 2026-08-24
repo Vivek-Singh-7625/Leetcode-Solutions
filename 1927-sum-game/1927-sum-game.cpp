64e500330fbe266750a38afe970b68e6a877525e
Vivek-Singh-7625 <vs514892@gmail.com>
2026-08-24 15:15:02 +0530

    Time: 3 ms (66.98%), Space: 13.8 MB (97.62%) - LeetHub

--- 1927-sum-game/1927-sum-game.cpp
+++ 1927-sum-game/1927-sum-game.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
     bool sumGame(string num) {
-        int a = 0 , b = 0 , c = 0 , d = 0 , n = num.length();
-        for(int i = 0 ; i < n ; i++){
-            if(num[i] != '?'){
-                if(i < n/2) a += (num[i] - '0');
-                else    b += (num[i] - '0');
+        int n = num.size();
+        int leftSum = 0, rightSum = 0;
+        int leftAlice = 0, leftBob = 0;
+        int rightAlice = 0, rightBob = 0;
+        bool aliceTurn = true;
+
+        for (int i = 0; i < n; i++) {
+            if (num[i] >= '0' && num[i] <= '9') {
+                if (i < n / 2)  leftSum += num[i] - '0';
+                else    rightSum += num[i] - '0';
             }
-            else{
-                if(i < n/2) c++;
-                else    d++;
+            else {
+                if (i < n / 2) {
+                    if (aliceTurn)  leftAlice++;
+                    else    leftBob++;
+                }
+                else {
+                    if (aliceTurn)  rightAlice++;
+                    else    rightBob++;
+                }
+                aliceTurn = !aliceTurn;
             }
         }
-        return (c+d)&1 || (2*(a-b) != 9*(d-c));
+        return (leftSum + leftAlice * 9 != rightSum + rightBob * 9) ||
+               (leftSum + leftBob * 9 != rightSum + rightAlice * 9);
     }
 };
