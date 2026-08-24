bbc1e25f71412afbaa80bd942de2d86ec19c9f38
Vivek-Singh-7625 <vs514892@gmail.com>
2026-08-24 15:13:05 +0530

    Time: 7 ms (21.62%), Space: 13.9 MB (91.92%) - LeetHub

--- 1927-sum-game/1927-sum-game.cpp
+++ 1927-sum-game/1927-sum-game.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     bool sumGame(string num) {
-        int a = 0 , b = 0 , c = 0 , d = 0 , n = num.length() , x = 1 , e = 0 , f = 0;
+        int a = 0 , b = 0 , c = 0 , d = 0 , n = num.length();
         for(int i = 0 ; i < n ; i++){
-            if(num[i] >= '0' and num[i] <= '9'){
+            if(num[i] != '?'){
                 if(i < n/2) a += (num[i] - '0');
                 else    b += (num[i] - '0');
             }
             else{
-                if(i < n/2){
-                    if(x)   c++;
-                    else    d++;
-                }
-                else{
-                    if(x)   e++;
-                    else    f++;
-                }
-                x = x^1;
+                if(i < n/2) c++;
+                else    d++;
             }
         }
-        return (a + c*9 != b + f*9) || (a + d*9 != b + e*9);
+        return (c+d)&1 || (2*(a-b) != 9*(d-c));
     }
 };
