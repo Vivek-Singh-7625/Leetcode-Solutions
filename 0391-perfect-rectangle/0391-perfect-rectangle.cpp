a632bea2ae8da30caaf897f003475d08053f7dbe
Vivek-Singh-7625 <vs514892@gmail.com>
2026-08-25 15:33:08 +0530

    Time: 41 ms (73.65%), Space: 36.3 MB (62.17%) - LeetHub

--- 0391-perfect-rectangle/0391-perfect-rectangle.cpp
+++ 0391-perfect-rectangle/0391-perfect-rectangle.cpp
@@ -6,8 +6,8 @@ public:
         vector<int> id(4,INT_MAX);
         id[2] = INT_MIN;
         id[3] = INT_MIN;
-        set<string> s;
-        string a , b , c , d;
+        set<pair<int,int>> s;
+        pair<int,int> a , b , c , d;
         if(m == 1)  return true;
         for(int i = 0 ; i < m ; i++){    
             ans += abs(1LL*(rectangles[i][0]-rectangles[i][2])*(rectangles[i][1]-rectangles[i][3]));
@@ -16,20 +16,20 @@ public:
             id[2] = max(id[2],rectangles[i][2]);
             id[3] = max(id[3],rectangles[i][3]);
 
-            a = to_string(rectangles[i][0]) + " " + to_string(rectangles[i][1]);
-            b = to_string(rectangles[i][2]) + " " + to_string(rectangles[i][3]);
-            c = to_string(rectangles[i][0]) + " " + to_string(rectangles[i][3]);
-            d = to_string(rectangles[i][2]) + " " + to_string(rectangles[i][1]);
+            a = {rectangles[i][0],rectangles[i][1]};
+            b = {rectangles[i][2],rectangles[i][3]};
+            c = {rectangles[i][0],rectangles[i][3]};
+            d = {rectangles[i][2],rectangles[i][1]};
 
             if(s.erase(a) == 0) s.insert(a);
             if(s.erase(b) == 0) s.insert(b);
             if(s.erase(c) == 0) s.insert(c);
             if(s.erase(d) == 0) s.insert(d);
         }
-        a = to_string(id[0]) + " " + to_string(id[1]);
-        b = to_string(id[2]) + " " + to_string(id[3]);
-        c = to_string(id[0]) + " " + to_string(id[3]);
-        d = to_string(id[2]) + " " + to_string(id[1]);
+        a = {id[0],id[1]};
+        b = {id[2],id[3]};
+        c = {id[0],id[3]};
+        d = {id[2],id[1]};
         if(s.size() != 4)   return false;
         for(auto& v : s){
             if(v == a || v == b || v == c || v == d)   continue;
