dff65eb1fd3b2c4ef5e201b595e24de23c583fe0
Vivek-Singh-7625 <vs514892@gmail.com>
2026-09-05 17:56:07 +0530

    Time: 37 ms (61.31%), Space: 32.9 MB (14.94%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.cpp
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -5,18 +5,25 @@ public:
         if(n%groupSize)   return false;
         priority_queue<int,vector<int>,greater<int>> p , q;
         unordered_map<int,int> mpp;
+
         for(int i = 0 ; i < n ; i++)    mpp[hand[i]]++;
+
         for(auto& [y,v] : mpp){
             p.push(y);
         }
+        
         int t , k = 0;
         vector<int> a(groupSize);
+
         while(!p.empty()){
             t = p.top();
             mpp[t]--;
             p.pop();
+
             if(mpp[t])  q.push(t);
+
             a[k%groupSize] = t;
+            
             if( !((k+1) % groupSize) ){
                 while(!q.empty()){
                     p.push(q.top());
