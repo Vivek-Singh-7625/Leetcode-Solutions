6292496b8d5f21860ac6e2d536416a5b37f791f2
Vivek-Singh-7625 <vs514892@gmail.com>
2026-09-09 12:36:30 +0530

    Time: 0 ms (100%), Space: 9.3 MB (7.73%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long c = log2(n)/3 , ans = 0 , k = 0;
+        unsigned long long a = 1;
+        while(a*1000 <= n){
+            ans += (k*(a*1000 - a));
+            a = a*1000;
+            k++;
+        }
+        if(n >= a)  ans += k*(n-a+1);
+        return ans;
+    }
+};
