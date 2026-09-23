35a0d7078d4b08e7abb3bc7cbad9734364c9ee2c
Vivek-Singh-7625 <vs514892@gmail.com>
2026-09-23 12:13:41 +0530

    Time: 1 ms (78.07%), Space: 102.3 MB (65.24%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
-        int n = nums.size() , ans = INT_MAX , val , search , idx;
-        vector<int> p(n+1,0) , s(n+1,0);
+        int n = nums.size() , ans = 0 , val = 0 , t = 0;
+        for(int i = 0 ; i < n ; i++)    val += nums[i];
+        if(val == x)    return n;
+        val -= x;
+        if(val < 0) return -1;
+        int l = 0;
         for(int i = 0 ; i < n ; i++){
-            p[i+1] = p[i] + nums[i];
-            s[i+1] = s[i] + nums[n-i-1];
-        } 
-        for(int i = 0 ; i <= n ; i++){
-            val = p[i];
-            search = x - p[i];
-            if(search < 0)  break;
-            auto it = upper_bound(s.begin(),s.end(),search);
-            idx = it - s.begin();
-            if(idx < 0 or idx > n)  continue;
-            if(val + s[idx-1] == x){
-                ans = min(ans,i+idx-1);
+            t += nums[i];
+            while(t > val){
+                t -= nums[l++];
             }
+            if(t == val)    ans = max(ans,i-l+1);
         }
-        return (ans == INT_MAX or ans > n) ? -1 : ans;
+        if(ans == 0)    return -1;
+        return n-ans;
     }
 };
