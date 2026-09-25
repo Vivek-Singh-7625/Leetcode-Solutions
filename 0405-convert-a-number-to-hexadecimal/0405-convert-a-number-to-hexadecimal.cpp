6b02af4fbdbbcd3e0337fc03da588eee7324b580
Vivek-Singh-7625 <vs514892@gmail.com>
2026-09-25 15:48:28 +0530

    Time: 0 ms (100%), Space: 7.7 MB (73.12%) - LeetHub

--- 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.cpp
+++ 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.cpp
@@ -1,22 +1,15 @@
-class Solution {
+ class Solution {
 public:
     string toHex(int num) {
-        if(num == 0)    return "0";
-        long long k = 1 ;
-        unsigned int p = 1ll*INT_MAX*2 + 1;
-        if(num < 0) p = p + num + 1;
-        else    p = num;
-        int t , l = 1;
-        while(k < p)  k = k << 4 , l++;
-        if(k > p) k = k >> 4 , l--;
+        if (num == 0)   return "0";
+        string hex = "0123456789abcdef";
         string ans;
-        while(l--){
-            t = p/k;
-            p -= t*k;
-            k = k >> 4;
-            if(t >= 0 and t <= 9)   ans += ('0'+t);
-            else    ans += ('a'+t%10);
+        unsigned int n = num;
+        while (n > 0) {
+            ans += hex[n & 15];
+            n >>= 4;
         }
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
